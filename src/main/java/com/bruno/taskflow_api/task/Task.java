6ebcc9f1ebefd9dbbe5ad53e46b55884a4f2d5bf
Bruno Alexander Chafloque Cerna <bchafloquec15_2@unc.edu.pe>
2026-07-13 09:21:58 -0500

    [DAY-03] fix: Added the missing anootation

--- src/main/java/com/bruno/taskflow_api/task/Task.java
+++ src/main/java/com/bruno/taskflow_api/task/Task.java
@@ -10,11 +10,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
 @Entity
+@Table(name = "tasks")
 public class Task {
 
   @Id
